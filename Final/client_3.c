b5aa85fb5f060cdda52a4e00f0d9a50d5277e2bd
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-07-15 08:50:21 -0400

    final

--- Final/client_3.c
+++ Final/client_3.c
@@ -5,70 +5,146 @@
 #include <sys/msg.h>
 #include <sys/types.h>
 #include <unistd.h>
-
+#include <signal.h>
 #define MSG_SIZE 256
 
-typedef struct {
+typedef struct
+{
     long mtype;
     char mtext[MSG_SIZE];
     int client_id;
+    int receiver_id;
 } Message;
+int client_id = 0;
+int receiver_id = 0;
+void sigint_handler(int signo)
+{
+    printf("Exiting client\n");
+    // Get server's mq_id
+    key_t server_key = ftok("/tmp", 0);
+    int server_mq_id = msgget(server_key, 0);
+    if (server_mq_id == -1)
+    {
+        printf("Server Shutdown\n");
+        exit(0);
+    }
 
+    Message msg;
+    msg.mtype = 4;
+    msg.client_id = client_id;
+    sprintf(msg.mtext, "%d", client_id);
+    if (msgsnd(server_mq_id, &msg, sizeof(Message), 0) == -1)
+    {
+        printf("Server Shutdown\n");
+        exit(0);
+    }
+    exit(0);
+}
+void create_worker_thread(pid_t client_mq_id)
+{
+    // This thread for output
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        perror("Fork failed");
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        while (1)
+        {
 
+            Message msg;
 
-int main(int argc, char **argv) {
-    
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <client_id>\n", argv[0]);
+            if (msgrcv(client_mq_id, &msg, sizeof(Message), 0, 0) == -1)
+            {
+                perror("msgrcv failed");
+                exit(1);
+            }
+            // Not from server
+            if (msg.client_id != 0)
+            {
+                printf("%d: %s\n", msg.client_id, msg.mtext);
+            }
+        }
+    }
+}
+int main(int argc, char **argv)
+{
+    signal(SIGINT, sigint_handler);
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage: %s <client_id> <reciever_id>\\n", argv[0]);
         exit(1);
     }
 
+    client_id = atoi(argv[1]);
+    // Create client mailbox
+    key_t client_key = ftok("/tmp", client_id + 1000);
+    int client_mq_id = msgget(client_key, IPC_CREAT | 0666);
+    if (client_mq_id == -1)
+    {
+        perror("msgget failed");
+        exit(1);
+    }
+    printf("Client: %d\n", client_mq_id);
+
+    printf("Client mailbox created\n");
+
     // Get server's mq_id
-    key_t server_key = ftok("/tmp", 's');
+    key_t server_key = ftok("/tmp", 0);
     int server_mq_id = msgget(server_key, 0);
-    if (server_mq_id == -1) {
+    if (server_mq_id == -1)
+    {
         perror("msgget failed");
         exit(1);
     }
 
     // Inform server of client_id
     Message msg;
+    msg.client_id = client_id;
     msg.mtype = 1;
-    msg.client_id = atoi(argv[1]);
-    sprintf(msg.mtext, "%d", atoi(argv[1]));
-    if (msgsnd(server_mq_id, &msg, sizeof(Message) , 0) == -1) {
+    msg.receiver_id = 0;
+
+    sprintf(msg.mtext, "%d", client_mq_id);
+    if (msgsnd(server_mq_id, &msg, sizeof(Message), 0) == -1)
+    {
         perror("msgsnd failed");
         exit(1);
     }
     printf("Sent client_id to server\n");
-    
-    // Create client mailbox
-    key_t client_key = ftok("/tmp", atoi(argv[1]));
-    int client_mq_id = msgget(client_key, IPC_CREAT | 0666);
-    if (client_mq_id == -1) {
-        perror("msgget failed");
+
+    printf("Waiting for server to send client_id\n");
+
+    if (msgrcv(client_mq_id, &msg, sizeof(Message), 0, 0) == -1)
+    {
+
+        perror("msgrcv failed");
         exit(1);
     }
-
-    printf("Client mailbox created\n");
-
-    while (1) {
-        printf("Enter a message to send to the server: ");
-        fgets(msg.mtext, MSG_SIZE, stdin);
+    printf("Received client_id from server: %s\n", msg.mtext);
+    int server_client_mq_id = atoi(msg.mtext);
+    create_worker_thread(client_mq_id);
+    while (1)
+    {
+        msg.client_id = client_id;
+        printf("Enter a message to send to the server: \n");
         msg.mtype = 2;
-        if (msgsnd(server_mq_id, &msg, sizeof(Message), 0) == -1) {
+
+        scanf("%s", msg.mtext);
+        /*fflush(stdin);
+        printf("Enter receiver id: \n");
+
+        scanf("%d", &msg.receiver_id);*/
+        msg.receiver_id = atoi(argv[2]);
+        if (msgsnd(server_client_mq_id, &msg, sizeof(Message), 0) == -1)
+        {
             perror("msgsnd failed");
             exit(1);
         }
-        
-        // Receive a response
-        if (msgrcv(client_mq_id, &msg, sizeof(Message), getpid(), 0) == -1) {
-            perror("msgrcv failed");
-            exit(1);
-        }
-        printf("Received response from server: %s\n", msg.mtext);
 
-        // Code for user to exit process here, e.g using a specific input string
+        printf("\n");
+        sleep(1);
     }
 
     return 0;
--- Final/server_3.c
+++ Final/server_3.c
@@ -7,7 +7,6 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
-
 #define MAX_CLIENTS 10
 #define MSG_SIZE 256
 
@@ -18,59 +17,90 @@ mtype
 3 - response
 4 - termination
 */
-typedef struct {
+typedef struct
+{
     long mtype;
     char mtext[MSG_SIZE];
     int client_id;
+    int receiver_id;
 } Message;
 
-
-typedef struct {
+typedef struct
+{
     pid_t client_pid;
     int client_mq_id;
     int worker_thread_id;
+    int client_id;
 } Client;
-
 
 Client clients[MAX_CLIENTS];
 int num_clients = 0;
 int server_mq_id;
 
+int mutex=0;
 
-void create_worker_thread(pid_t client_pid, int client_mq_id) {
-    printf("Creating worker thread for client %d\n", client_mq_id);
+void create_worker_thread(pid_t client_real_mq_id, int client_server_mq_id)
+{
+    printf("Creating worker thread for client %d\n", client_server_mq_id);
     pid_t pid = fork();
-    if (pid < 0) {
+    if (pid < 0)
+    {
         perror("Fork failed");
         exit(1);
-    } else if (pid == 0) {
+    }
+    else if (pid == 0)
+    {
         // Worker thread code
-        while (1) {
+        while (1)
+        {
             Message msg;
-            if (msgrcv(client_mq_id, &msg, sizeof(Message), getpid(), 0) == -1) {
-                printf("client reached here\n");
+            if (msgrcv(client_server_mq_id, &msg, sizeof(Message), 0, 0) == -1)
+            {
                 perror("msgrcv failed");
                 exit(1);
             }
             printf("==Received message from client %d: %s\n", msg.client_id, msg.mtext);
-            // Process the received message here
-            
-            // Send a response back to the client
-            strcpy(msg.mtext, "This is a response from the server");
-            msg.mtype = client_pid;
-            if (msgsnd(client_mq_id, &msg, sizeof(Message) - sizeof(long), 0) == -1) {
-                perror("msgsnd failed");
-                exit(1);
+            // Processing the received message here
+
+            if (msg.mtype == 2)
+            {
+                while (mutex==1){sleep(0.1);};
+                mutex=1;
+                
+                int receiver_id = msg.receiver_id;
+
+                for (int i = 0; i < num_clients; i++)
+                {
+                    if (clients[i].client_id == receiver_id)
+                    {
+                        msg.mtype = 2;
+                        if (msgsnd(clients[i].client_mq_id, &msg, sizeof(Message), 0) == -1)
+                        {
+                            perror("msgsnd failed");
+                            exit(1);
+                        }
+                        break;
+                    }
+                }
+
+                mutex=0;
+
+            }
+            else if (msg.mtype == 4)
+            {
+                printf("Client %d is shutting down...\n", msg.client_id);
+                exit(0);
             }
         }
     }
 }
 
-
-void handle_sigint(int sig) {
+void handle_sigint(int sig)
+{
     printf("Server is shutting down...\n");
-    //kill all clients
-    for (int i = 0; i < num_clients; i++) {
+    // kill all clients
+    for (int i = 0; i < num_clients; i++)
+    {
         kill(clients[i].client_pid, SIGINT);
         msgctl(clients[i].client_mq_id, IPC_RMID, NULL);
     }
@@ -78,65 +108,115 @@ void handle_sigint(int sig) {
     exit(0);
 }
 
-
-int main() {
+int main()
+{
     signal(SIGINT, handle_sigint);
 
-
     // Create server mailbox
-    key_t server_key = ftok("/tmp", 's');
+    key_t server_key = ftok("/tmp", 0);
     server_mq_id = msgget(server_key, IPC_CREAT | 0666);
 
-    if (server_mq_id == -1) {
+    if (server_mq_id == -1)
+    {
         perror("msgget failed");
         exit(1);
     }
 
-
     printf("Server is running...\n");
 
+    while (1)
+    {
 
-    while (1) {
-       
-       printf("Waiting for message from client...\n");
+        printf("Waiting for message from client...\n");
         Message msg;
-        if (msgrcv(server_mq_id, &msg, sizeof(Message), 1, 0) == -1) {
+
+        if (msgrcv(server_mq_id, &msg, sizeof(Message), 1, 0) == -1)
+        {
+
             perror("msgrcv failed");
             exit(1);
         }
 
         printf("Received message from client\n");
-        if(msg.mtype==1){
-            pid_t client_pid = atoi(msg.mtext);
-            
-            
-            // Create client mailbox
-            key_t client_key = ftok("/tmp", client_pid);
-            int client_mq_id = msgget(client_key, IPC_CREAT | 0666);
-            if (client_mq_id == -1) {
+        printf("Message type: %ld\n", msg.mtype);
+        if (msg.mtype == 1)
+        {
+            // Client registration
+            pid_t client_mq_id = atoi(msg.mtext);
+
+            printf("Client mailbox id: %d\n", client_mq_id);
+
+            // Create client mailbox for server to recieve
+            key_t client_key = ftok("/tmp", msg.client_id);
+            int client_server_mq_id = msgget(client_key, IPC_CREAT | 0666);
+            if (client_server_mq_id == -1)
+            {
                 perror("msgget failed");
                 exit(1);
             }
-            
-
-
+            //while (mutex==1){sleep(0.1);};
+           // mutex=1;
             // Register the client
-            clients[num_clients].client_pid = client_pid;
+            clients[num_clients].client_pid = atoi(msg.mtext);
             clients[num_clients].client_mq_id = client_mq_id;
-            create_worker_thread(client_pid, client_mq_id);
+            clients[num_clients].client_id = msg.client_id;
+            create_worker_thread(atoi(msg.mtext), client_server_mq_id);
 
             printf("Number of clients %d\n", num_clients);
             num_clients++;
+
+            // send mailbox adress to client
+
+            msg.receiver_id = msg.client_id;
+            msg.client_id = 0;
+            msg.mtype = 3;
+
+            sprintf(msg.mtext, "%d", client_server_mq_id);
+            if (msgsnd(client_mq_id, &msg, sizeof(Message), 0) == -1)
+            {
+                perror("msgsnd failed");
+                exit(1);
+            }
+            printf("Sent response to client\n");
+            //mutex=0;
         }
-        else if(msg.mtype==2){
-            pid_t client_pid = atoi(msg.mtext);
-            for(int i=0;i<num_clients;i++){
-                //to all clients
-                if(clients[i].client_pid==client_pid){
-                    kill(clients[i].worker_thread_id,SIGINT);
-                    msgctl(clients[i].client_mq_id, IPC_RMID, NULL);
-                    for(int j=i;j<num_clients-1;j++){
-                        clients[j]=clients[j+1];
+        else if (msg.mtype == 2)
+        {
+            printf("===Message\n");
+            // Client message
+            int client_id = msg.client_id;
+            char *message = msg.mtext;
+            printf("%d: %s", client_id, message);
+            msg.client_id = 0;
+            msg.mtype = 3;
+            strcpy(msg.mtext, "ok");
+            if (msgsnd(server_mq_id, &msg, sizeof(Message), 0) == -1)
+            {
+                perror("msgsnd failed");
+                exit(1);
+            }
+        }
+        else if (msg.mtype == 3)
+        {
+            // Client response
+            int client_id = msg.client_id;
+            char *message = msg.mtext;
+            printf("%d: %s", client_id, message);
+        }
+        else if (msg.mtype == 4)
+        {
+            // Client termination
+
+            int client_id = msg.client_id;
+            printf("Client %d terminated\n", client_id);
+
+            for (int i = 0; i < num_clients; i++)
+            {
+                if (clients[i].client_pid == client_id)
+                {
+                    for (int j = i; j < num_clients - 1; j++)
+                    {
+                        clients[j] = clients[j + 1];
                     }
                     num_clients--;
                     break;
@@ -144,7 +224,6 @@ int main() {
             }
         }
     }
-
 
     return 0;
 }
